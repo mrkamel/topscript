2a1bd8c803c9b6512f3192c0deecab47088f301a
Benjamin Vetter <benjamin.vetter@nymcard.com>
2025-04-25 19:40:29 +0200

    Add another test

--- index.spec.ts
+++ index.spec.ts
@@ -386,6 +386,12 @@ describe('topscript', () => {
         delete obj.a;
         obj
       `)).toEqual({ b: 2 });
+
+      expect(topscript(`
+        const obj = { a: { b: 1, c: 2 } };
+        delete obj.a.b;
+        obj
+      `)).toEqual({ a: { c: 2 } });
       
       expect(topscript(`
         const arr = [1, 2, 3];
